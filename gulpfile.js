e872ede61fe4fdd4587e3ddf38f4b9608dec73b9
manpenghui <1217430023@qq.com>
2016-07-18 21:42:33 +0800

    hdvfi

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('testAutoFx', function () {
             cascade: true, //是否美化属性值 默认：true 像这样：
             //-webkit-transform: rotate(45deg);
             //        transform: rotate(45deg);
-            remove:true //是否去掉不必要的前缀 默认：true
+            remove:false //是否去掉不必要的前缀 默认：true
         }))
         .pipe(gulp.dest('dist/css'));
 });
